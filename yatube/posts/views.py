50c27b9a1e54f7ed3e41bd459a07894f658a71a9
Ильин Артём <joker9099@mail.ru>
2021-12-21 21:05:31 +0300

    Подключение CSS

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,9 +4,15 @@ from django.shortcuts import render
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    context = {
+        'text': 'Это главная страница проекта Yatube'
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    return render(request, template)
+    context = {
+        'text': 'Здесь будет информация о группах проекта Yatube'
+    }
+    return render(request, template, context)
