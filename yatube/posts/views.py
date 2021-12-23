a9272a851bc9fb0bb296158130e399ff6ea9beee
Ильин Артём <joker9099@mail.ru>
2021-12-23 20:45:05 +0300

    Загрузка тестовых данных

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
+
+
+# def index(request):
+#     template = 'posts/index.html'
+#     context = {
+#         'text': 'Это главная страница проекта Yatube'
+#     }
+#     return render(request, template, context)
 
 
 def index(request):
-    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'text': 'Это главная страница проекта Yatube'
+        'posts': posts,
     }
-    return render(request, template, context)
-
+    return render(request, 'posts/index.html', context) 
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
