c262c267bbcbe8bdb0a335362fde250ac56dd6b5
Ильин Артём <joker9099@mail.ru>
2021-12-23 23:20:24 +0300

    Ввод информации из базы данных

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -12,17 +12,17 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts'
     )
-    # group = models.ForeignKey(
-    #     User,
-    #     blank=True,
-    #     null=True,
-    #     on_delete=models.CASCADE
-    # )
+    group = models.ForeignKey(
+        'group',
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE
+    )
 
-# class Group(models.Model):
-#     title = models.CharField(max_length=200)
-#     slug = models.SlugField(max_length=400)
-#     description = models.TextField()
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=400)
+    description = models.TextField()
 
-#     def __str__(self):
-#         return self.title
+    def __str__(self):
+        return self.title
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 # def index(request):
@@ -20,7 +19,10 @@ def index(request):
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'text': 'Здесь будет информация о группах проекта Yatube'
+        'group': group,
+        'posts': posts,
     }
     return render(request, template, context)
