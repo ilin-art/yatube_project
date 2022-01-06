80307cc78fcb12b81bac8df951200cd3032154d6
Ильин Артём <joker9099@mail.ru>
2022-01-06 18:49:40 +0300

    Доработана страница регистрации

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('auth/', include('users.urls', namespace='users')),
     path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
+    path('auth/', include('django.contrib.auth.urls')),
 ]
