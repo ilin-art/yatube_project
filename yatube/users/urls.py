9a982c8ff147b4004b99d5b671c39a8c332826af
Ильин Артём <joker9099@mail.ru>
2022-01-07 04:14:23 +0300

    Шаблоны странцы входа

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+
 from . import views
 
 app_name = 'users'
@@ -14,5 +15,10 @@ urlpatterns = [
         'signup/',
         views.SignUp.as_view(),
         name='signup'
-    )
+    ),
+    path(
+        'login/',
+        LoginView.as_view(template_name='users/login.html'),
+        name='login'
+    ),
 ] 
