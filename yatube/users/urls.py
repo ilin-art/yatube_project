adf2f4fd7cd646cace2b6ef30f5efce6bd4b783f
Ильин Артём <joker9099@mail.ru>
2022-01-07 22:19:18 +0300

    Добавлена страница изменения пароля

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
 from django.urls import path
 
 from . import views
@@ -21,4 +21,18 @@ urlpatterns = [
         LoginView.as_view(template_name='users/login.html'),
         name='login'
     ),
-] 
+    path(
+        'password_change/',
+        PasswordChangeView.as_view(
+            template_name='users/password_change_form.html'
+            ),
+        name='password_change'
+    ),
+    path(
+        'password_change/done/',
+        PasswordChangeDoneView.as_view(
+            template_name='users/password_change_done.html'
+            ),
+        name='password_change_done'
+    ),
+]
