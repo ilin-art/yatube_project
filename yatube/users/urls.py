ed9122f0c31b173a5b681dfaeb1bd371c746f849
Ильин Артём <joker9099@mail.ru>
2022-01-08 19:49:30 +0300

    Добавлены страницы по восстановлению пароля

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,4 +1,7 @@
-from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.views import (
+    LoginView, LogoutView, PasswordChangeView,
+    PasswordChangeDoneView, PasswordResetView, PasswordResetDoneView,
+    PasswordResetConfirmView, PasswordResetCompleteView)
 from django.urls import path
 
 from . import views
@@ -34,5 +37,33 @@ urlpatterns = [
             template_name='users/password_change_done.html'
             ),
         name='password_change_done'
+    ),
+    path(
+        'password_reset/',
+        PasswordResetView.as_view(
+            template_name='users/password_reset_form.html'
+            ),
+        name='password_reset_form'
+    ),
+    path(
+        'password_reset/done/',
+        PasswordResetDoneView.as_view(
+            template_name='users/password_reset_done.html'
+            ),
+        name='password_reset_done'
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        PasswordResetConfirmView.as_view(
+            template_name='users/password_reset_confirm.html'
+            ),
+        name='password_reset_confirm'
+    ),
+    path(
+        'reset/done/',
+        PasswordResetCompleteView.as_view(
+            template_name='users/password_reset_complete.html'
+            ),
+        name='password_reset_complete'
     ),
 ]
